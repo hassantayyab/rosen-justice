e0f415e688491d7b0341ac3eaadbb0c7464ae9dc
hassantayyab <hassandogar9@gmail.com>
2021-04-24 00:09:48 +0500

    added ie11 fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,29 +1,37 @@
-
-
-
-
 const injectMakerWidgetScript = () => {
-    function addJS() {
-      const s = document.createElement(`script`);
-      s.type = `text/javascript`;
-      s.src = "https://scripts.iconnode.com/84616.js";
-      document.getElementsByTagName(`head`)[0].appendChild(s);
-    }
-    addJS();
-  };
-  
-  const addScript = url => {
-    const script = document.createElement("script")
-    script.src = url
-    script.async = true
-    document.body.appendChild(script)
-  }
-  
-  exports.onClientEntry = (s) => {
-    addScript("https://localreviews.buzz/embed/v4/161843050721504/3/1234567935183")
-    injectMakerWidgetScript();
-  };
-  exports.onPreRouteUpdate = (s) => {
-    addScript("https://localreviews.buzz/embed/v4/161843050721504/3/1234567935183")
-    injectMakerWidgetScript();
-  };
+	function addJS() {
+		const s = document.createElement(`script`)
+		s.type = `text/javascript`
+		s.src = 'https://scripts.iconnode.com/84616.js'
+		document.getElementsByTagName(`head`)[0].appendChild(s)
+	}
+	addJS()
+}
+
+const loadPolyfills = () => {
+	if (typeof window.IntersectionObserver === 'undefined') {
+		import('intersection-observer')
+	}
+}
+
+const addScript = (url) => {
+	const script = document.createElement('script')
+	script.src = url
+	script.async = true
+	document.body.appendChild(script)
+}
+
+exports.onClientEntry = (s) => {
+	addScript(
+		'https://localreviews.buzz/embed/v4/161843050721504/3/1234567935183'
+	)
+	injectMakerWidgetScript()
+
+	loadPolyfills()
+}
+exports.onPreRouteUpdate = (s) => {
+	addScript(
+		'https://localreviews.buzz/embed/v4/161843050721504/3/1234567935183'
+	)
+	injectMakerWidgetScript()
+}
